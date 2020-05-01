ccb1310335950d1b44a0bed629f8e6d654e62139
Raziq Noorali <rnoorali@nanna.uchicago.edu>
2020-05-01 11:33:01 -0500

    Added tools folder and working on adding custom fields

--- Notebook/raziq_croc.py
+++
@@ -1,7 +0,0 @@
-import yt as yt
-
-ds = yt.load("~/Data/rei20c1_a0.1667/rei20c1_a0.1667.art")
-
-def _H1_number_density(field, data):
-    return data["H1"] * data["thermal_energy"] * data["density"]
-ds.add_field(("gas", "H1_number_density"), function=_raziqpressure, units="1/cm**3")
