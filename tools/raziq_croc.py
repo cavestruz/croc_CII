2a70b6dce38a304c369305179ebcd626a477494d
Raziq Noorali <rnoorali@nanna.uchicago.edu>
2020-05-15 07:52:29 -0500

    Updated Slice plot generator

--- tools/raziq_croc.py
+++ tools/raziq_croc.py
@@ -7,6 +7,18 @@ ds = yt.load("~/Data/rei20c1_a0.1667/rei20c1_a0.1667.art")
 
 plot_list = ['HI number density','HII number density','HeI number density','HeII number density','HeIII number density','log_dust_attenuation','rC2e','rC2a','C2_e_cooling','C2_a_cooling', 'C2_HeI_cooling']
 
-for p in plot_list:
-    slc = yt.SlicePlot(ds, 'z',p)
-    slc.save(p + '.png')
+def make_slice_plot(dataset,list_or_array):
+    all_data_at_z_0 = dataset.r[:,:,0]
+    for p in list_or_array:
+        minimum = np.amin(all_data_at_z_0[p])
+        maximum = np.amax(all_data_at_z_0[p])
+        ratio = maximum/minimum
+        if ratio < 100:
+            slc = yt.SlicePlot(ds, 'z',p)
+            slc.set_log(p, True)
+            slc.save(p + '_sliceplot.png')
+        else: 
+            slc = yt.SlicePlot(ds, 'z',p)
+            slc.save(p + '_sliceplot.png')
+
+make_slice_plot(ds,plot_list)
