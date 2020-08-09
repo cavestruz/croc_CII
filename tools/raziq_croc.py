6b14ea0244e32ebd500dc31c354617d432a42690
Raziq Shaquille Noorali <rnoorali@gl-login2.arc-ts.umich.edu>
2020-08-08 22:41:18 -0400

    Can now make spherical plots, but needs a couple adjsutments

--- tools/raziq_croc.py
+++ tools/raziq_croc.py
@@ -16,7 +16,7 @@ def make_slice_plot(dataset,list_or_array,full_box=False):
 
         slc.save("../SlicePlots/"+ p + '_sliceplot.png')
 
-def make_histogram_slice(dataset,list_or_array):
+def make_histogram_slice(dataset,list_or_array,log_frequency=False):
 
     for p in list_or_array:
         minimum = np.amin(dataset['data_object'][p])
@@ -26,10 +26,10 @@ def make_histogram_slice(dataset,list_or_array):
             x,bin_edges = np.histogram(dataset['data_object'][p],bins = 100) 
             plt.bar(bin_edges[:-1], x,width = bin_edges[1]-bin_edges[0])
             plt.title(p)
-            plt.xlabel(p)
+            plt.xlabel(p+' '+str(dataset['data_object'][p].units))
             plt.ylabel('Frequency')
-            plt.savefig("../Histograms/"+ p + '_histogram.png')
-            plt.clf()
+            if log_frequency == True:
+                plt.yscale('log')
 
         else:
 
@@ -37,10 +37,12 @@ def make_histogram_slice(dataset,list_or_array):
             plt.bar(bin_edges[:-1], x, width = 1) 
             plt.xlim(min(bin_edges), max(bin_edges))
             plt.title(p )
-            plt.xlabel(p + " order of magnitude")
+            plt.xlabel('log10('+p+' '+str(dataset['data_object'][p].units))
             plt.ylabel('Frequency')
-            plt.savefig("../Histograms/"+ p + '_histogram.png')
-            plt.clf()
+            if log_frequency == True:
+                plt.yscale('log')
+        plt.savefig("../Histograms/"+ p + '_histogram.png')
+        plt.clf()
 def make_sphere_region(row_of_table):
     a = 1/(1+ds.current_redshift)
     h = ds.hubble_constant
@@ -57,12 +59,8 @@ ds = yt.load("~/Data/rei20c1_a0.1667/rei20c1_a0.1667.art")
 
 #all_data_at_z_0 = ds.r[:,:,0]
 
-plot_list = ['HI number density','HII number density','HeI number density','HeII number density','HeIII number density','log_dust_attenuation','rCIIe','rCIIa','CII_e_cooling','CII_a_cooling', 'CII_HeI_cooling', 'CII_CMB_emission','CII_H2_ortho', 'CII_H2_para']
-#plot_list = ['CII_H2_ortho', 'CII_H2_para']
-
-#make_slice_plot(all_data_at_z_0,plot_list)
-
-#make_histogram_slice(all_data_at_z_0,plot_list)
+#plot_list = ['HI number density','HII number density','HeI number density','HeII number density','HeIII number density','log_dust_attenuation','rCIIe','rCIIa','CII_e_cooling','CII_a_cooling', 'CII_HeI_cooling', 'CII_CMB_emission','CII_H2_ortho', 'CII_H2_para']
+plot_list = ['log_dust_attenuation','rCIIe','rCIIa']
 
 halo_table = Table.read('/home/rnoorali/Data/halo_catalogs/out_14.list',format = "ascii.commented_header")
 
@@ -72,5 +70,5 @@ largest_mass = halo_table[halo_table.loc_indices[np.amax(halo_table['Mvir'])]]
 
 sphere = make_sphere_region(largest_mass)
 
-make_slice_plot(sphere,plot_list)
-make_histogram_slice(sphere,plot_list)
+#make_slice_plot(sphere,plot_list)
+make_histogram_slice(sphere,plot_list,log_frequency=True)
